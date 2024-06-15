71b9b7e5b57247659ab3339e20cac601b02f3942
HectorV <hectorfabiovasquez@gmail.com>
2024-06-15 14:32:07 -0500

    Added advanced command: execute()

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,4 +1,20 @@
-describe("Test suite", () => {
+describe("execute", () => {
+  beforeEach(async () => {
+    await browser.url(
+      "https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard"
+    );
+  });
+
+  it("execute() with params", async () => {
+    const doctor = await $("a[href='#/doctor-details/5']");
+    await browser.execute(function (doctor) {
+      doctor.style.border = "red solid 3px";
+    }, doctor);
+    await browser.pause(3000);
+  });
+});
+
+/* describe("Test suite", () => {
   it("First test", async () => {
     await browser.url(
       "https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard"
@@ -21,4 +37,4 @@ describe("Test suite", () => {
     const emailError = await $("label#Email-info");
     expect(await emailError.getText()).toEqual("Enter valid email");
   });
-});
+}); */
